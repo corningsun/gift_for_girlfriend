6410f1b0efd99a346864f1334f90ea0bfc79aeb1
Corning Sun <corningsun@163.com>
2018-07-11 16:16:17 +0800

    support year

--- love/functions.js
+++ love/functions.js
@@ -40,21 +40,31 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+
+	var years = Math.floor(seconds / (365 * 3600 * 24));
+
+  seconds = seconds % (365 * 3600 * 24);
+  var days = Math.floor(seconds / (3600 * 24));
+  if (days < 10) {
+    days = "0" + days;
+  }
+
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
+
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
+
 	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = " <span class=\"digit\">" + years + " 年 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
 	$("#clock").html(result);
 }
